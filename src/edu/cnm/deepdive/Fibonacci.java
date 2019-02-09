ba2756afacc1ef897f79411ba632afa996658e58
Felipe Cook <felipepcook@gmail.com>
2019-02-09 15:05:39 -0700

    Added functional Fibonacci sequence generator and solved even sum

--- src/edu/cnm/deepdive/Fibonacci.java
+++ src/edu/cnm/deepdive/Fibonacci.java
@@ -2,11 +2,42 @@ package edu.cnm.deepdive;
 
 public class Fibonacci {
 
+//  public static void main(String[] args) {
+//    long prevSumOfFibonacci = 0;
+//    long currSumOfFibonacci = 1;
+//    long evenSumOfFibonacci = 0;
+//    long finalSumOfEven = 0;
+//    do {
+//      if (sumOfFibonacci % 2 == 0) {
+//        evenSumOfFibonacci = sumOfFibonacci;
+//        finalSumOfEven += evenSumOfFibonacci;
+//      }
+//      sumOfFibonacci += sumOfFibonacci;
+//    } while (sumOfFibonacci <= 4_000_000);
+//
+//    System.out.println(sumOfFibonacci);
+//    System.out.println(evenSumOfFibonacci);
+//    System.out.println(finalSumOfEven);
+//  }
+
   public static void main(String[] args) {
-    long sumOfFibonacci = 1;
+    long prev = 0;
+    long curr = 1;
+    long temp = 0;
+    long evenTemp = 0;
+    long currEvenTemp = 0;
     do {
-      sumOfFibonacci += sumOfFibonacci;
-    } while (sumOfFibonacci < 4_000_000);
-    System.out.println(sumOfFibonacci);
+      temp = prev + curr;
+      if (temp % 2 == 0) {
+        evenTemp = temp;
+        currEvenTemp += evenTemp;
+      }
+      prev = curr;
+      curr = temp;
+    } while (temp < 4_000_000);
+    System.out.println(temp);
+    System.out.println(evenTemp);
+    System.out.println(currEvenTemp);
   }
+
 }
